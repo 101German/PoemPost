2f141e4ef8c26808e452ba48f82feb3f74f6478e
101German <gervika@list.ru>
2021-10-11 16:45:42 +0300

    Implement CommentRepository interface

---
+++ PoemPost.Data/Repositories/CommentRepository.cs
@@ -0,0 +1,18 @@
+﻿using PoemPost.Data.Interfaces;
+using PoemPost.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoemPost.Data.Repositories
+{
+    public class CommentRepository : BaseRepository<Comment>, ICommentRepository
+    {
+        public CommentRepository(RepositoryContext RepositoryContext) : base(RepositoryContext)
+        {
+
+        }
+    }
+}
