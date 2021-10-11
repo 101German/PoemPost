6b85a0f247080ccea359e1414ab6d35f7ee0a792
101German <gervika@list.ru>
2021-10-11 16:43:53 +0300

    Add LikeRepository interface

---
+++ PoemPost.Data/Interfaces/ILikeRepository.cs
@@ -0,0 +1,12 @@
+﻿using PoemPost.Data.Models;
+using System.Threading.Tasks;
+
+namespace PoemPost.Data.Interfaces
+{
+    public interface ILikeRepository 
+    { 
+        bool AddLike(Like like);
+        Task<int> GetLikesCountByPostIdAsync(int id);
+
+    }
+}
