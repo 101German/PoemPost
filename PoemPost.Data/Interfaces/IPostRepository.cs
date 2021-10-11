f6cc83cfb8541091a09351eb5c88a9943f5f1575
101German <gervika@list.ru>
2021-10-11 16:44:13 +0300

    Add PostRepository interface

---
+++ PoemPost.Data/Interfaces/IPostRepository.cs
@@ -0,0 +1,11 @@
+﻿using PoemPost.Data.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PoemPost.Data.Interfaces
+{
+    public interface IPostRepository : IBaseRepository<Post>
+    {
+
+    }
+}
