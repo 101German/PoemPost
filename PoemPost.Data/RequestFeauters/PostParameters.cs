4e2e4e8de0f688c1fa86b686fd41270bd5c120c3
101German <gervika@list.ru>
2021-10-13 16:53:15 +0300

    Add PostParameters

---
+++ PoemPost.Data/RequestFeauters/PostParameters.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PoemPost.Data.RequestFeauters
+{
+    public class PostParameters : RequestParameters
+    {
+        public ICollection<string> Authors { get; set; }
+        public string SearchTerm { get; set; }
+        public DateTime FromDateTime { get; set; } = DateTime.MinValue;
+        public DateTime ToDateTime { get; set; } = DateTime.MaxValue;
+    }
+}
