67af52b5a889764f6c1503b8955e095adac5c995
German <geras210e@gmail.com>
2021-10-17 22:06:20 +0300

    Add controllers

---
+++ PoemPost.Host/Controllers/CommentController.cs
@@ -0,0 +1,44 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using PoemPost.App.Commands.Create;
+using PoemPost.App.Commands.Delete;
+using PoemPost.Data.DTO;
+using System.Threading.Tasks;
+
+namespace PoemPost.Host.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CommentController : ControllerBase
+    {
+
+        private readonly IMediator _mediator;
+
+        public CommentController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CommentForCreationDTO comment)
+        {
+            var commentDTO = await _mediator.Send(new CreateCommentCommand()
+            {
+                Comment = comment
+            });
+
+            return Ok(commentDTO);
+        }
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _mediator.Send(new DeleteCommentCommand()
+            {
+                Id = id
+            });
+
+            return NoContent();
+        }
+
+    }
+}
---
+++ PoemPost.Host/Controllers/PostController.cs
@@ -0,0 +1,94 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PoemPost.App.Commands;
+using PoemPost.App.Commands.Update;
+using PoemPost.App.Queries;
+using PoemPost.App.Queries.Post;
+using PoemPost.Data.DTO;
+using PoemPost.Data.RequestFeauters;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PoemPost.Host.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PostController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper; 
+
+        public PostController(IMediator mediator,IMapper mapper)
+        {
+            _mediator = mediator;
+            _mapper = mapper;
+        }
+
+        [HttpGet("{id}",Name ="GetById")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var post =await _mediator.Send(new GetPostByIdQuery
+            {
+                Id = id,
+                TrackChanges = true
+            });
+
+            return Ok(post);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody]PostForCreationDTO post)
+        {
+            var postForReturn =await  _mediator.Send(new CreatePostCommand()
+            {
+                Post = post
+            }
+            );
+            
+            return CreatedAtRoute("GetById",new { id = postForReturn.Id},postForReturn);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery]PostParameters postParameters)
+        {
+            var postsEntities = await _mediator.Send(new GetFilteringPostsQuery()
+            {
+                PostParameters = postParameters,
+                TrackChanges = true
+            });
+
+            Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(postsEntities.MetaData));
+
+            var postsDTO = _mapper.Map<ICollection<PostDTO>>(postsEntities);
+
+            return Ok(postsDTO);
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _mediator.Send(new DeletePostCommand()
+            {
+                Id = id
+            });
+
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id,[FromBody]PostForUpdateDTO post)
+        {
+            await _mediator.Send(new UpdatePostCommand()
+            { 
+                Id=id,
+                Post=post
+            
+            });
+
+            return NoContent();
+        }
+    }
+}
