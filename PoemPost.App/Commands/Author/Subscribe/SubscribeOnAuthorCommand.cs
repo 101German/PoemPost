0cebde50ebd598f1645c6e4e87af75253e10b7ee
101German <gervika@list.ru>
2021-10-21 14:58:42 +0300

    Add SubscribeOnAuthorCommand

---
+++ PoemPost.App/Commands/Author/Subscribe/SubscribeOnAuthorCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Models;
+using PoemPost.Data.DTO;
+using PoemPost.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoemPost.App.Commands.Subscribe
+{
+    public class SubscribeOnAuthorCommand : IRequest
+    {
+        public UserForSubscription User { get; set; }
+    }
+}
