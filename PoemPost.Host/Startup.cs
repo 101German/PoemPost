756fda57d675a4ba0d3342e8dc75a94e7e5c2f23
101German <gervika@list.ru>
2021-10-21 15:05:19 +0300

    Fix ConfigureServices method

--- PoemPost.Host/Startup.cs
+++ PoemPost.Host/Startup.cs
@@ -24,7 +24,6 @@ namespace PoemPost.Host
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.ConfigurePostProcessors();
             services.ConfigureMassTransit();
             services.ConfigureAutoMapper();
             services.ConfigureValidators();
