f01e1b0df3d7a94dd95d0e7b10c8100cc2fc5e4f
101German <gervika@list.ru>
2021-10-21 18:31:36 +0300

    Add AuthorName in PostForCreationDTO

--- PoemPost.Data/DTO/Post/PostForCreationDTO.cs
+++ PoemPost.Data/DTO/Post/PostForCreationDTO.cs
@@ -4,6 +4,7 @@
     {
         public string Title { get; set; }
         public string PoemText { get; set; }
+        public string AuthorName { get; set; }
         public int AuthorId { get; set; }
     }
 }
