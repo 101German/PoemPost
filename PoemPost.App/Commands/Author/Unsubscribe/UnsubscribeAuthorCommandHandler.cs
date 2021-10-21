e03aa042a639517d786b5d0d2a1a44734fd554db
101German <gervika@list.ru>
2021-10-21 14:59:03 +0300

    Add UnsubscribeAuthorCommand

---
+++ PoemPost.App/Commands/Author/Unsubscribe/UnsubscribeAuthorCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using MassTransit;
+using MediatR;
+using PoemPost.App.Command.Unsubscribe;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PoemPost.App.Commands.Unsubscribe
+{
+    public class UnsubscribeAuthorCommandHandler : IRequestHandler<UnsubscribeAuthorCommand>
+    {
+        private readonly IPublishEndpoint _publishEndpoint;
+
+        public UnsubscribeAuthorCommandHandler(IPublishEndpoint publishEndpoint)
+        {
+            _publishEndpoint = publishEndpoint;
+        }
+
+        public async Task<Unit> Handle(UnsubscribeAuthorCommand request, CancellationToken cancellationToken)
+        {
+            await _publishEndpoint.Publish(request.User, cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
