4db6cdc66149aa45476a2d81bd51712decd914e2
101German <gervika@list.ru>
2021-11-16 16:11:18 +0300

    Update author profile

--- PoemPost.Data/Mapping/AuthorProfile.cs
+++ PoemPost.Data/Mapping/AuthorProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using PoemPost.Data.DTO;
 using PoemPost.Data.Models;
+using PoemPost.Data.RequestFeauters;
 
 namespace PoemPost.Data.Mapping
 {
@@ -9,8 +10,16 @@ namespace PoemPost.Data.Mapping
         public AuthorProfile()
         {
             CreateMap<AuthorForCreationDTO, Author>();
-            CreateMap<Author, AuthorDTO>();
+
+            CreateMap<Author, AuthorDTO>()
+                .ForMember(a=>a.PostsCount,opt=>opt
+                .MapFrom(a=>a.Posts.Count));
             CreateMap<AuthorForUpdateDTO, Author>();
+
+            CreateMap<PagedList<Author>, PagedList<AuthorDTO>>()
+              .ForMember(a => a.MetaData, opt => opt
+              .MapFrom(a => a.MetaData))
+              .ForMember(a => a.Items, opt => opt.MapFrom(a => a.Items));
         }
        
     }
