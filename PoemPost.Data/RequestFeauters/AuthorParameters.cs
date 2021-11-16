6ecc8ae57d1828064a92203e5f92eebf5eae7633
101German <gervika@list.ru>
2021-11-16 15:37:35 +0300

    Add author parameters

---
+++ PoemPost.Data/RequestFeauters/AuthorParameters.cs
@@ -0,0 +1,15 @@
+﻿using PoemPost.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoemPost.Data.RequestFeauters
+{
+    public class AuthorParameters : OrderParameters
+    {
+        public AuthorType AuthorType { get; set; }
+        public string SearchTerm { get; set; }
+    }
+}
