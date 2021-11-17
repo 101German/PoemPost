f805dd528591496d675789414e9d3c5ac857128e
101German <gervika@list.ru>
2021-11-17 13:38:00 +0300

    Remove unuseful namespaces

--- PoemPost.Data/RequestFeauters/SubscriptionParameters.cs
+++ PoemPost.Data/RequestFeauters/SubscriptionParameters.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PoemPost.Data.RequestFeauters
+﻿namespace PoemPost.Data.RequestFeauters
 {
     public class SubscriptionParameters : OrderParameters
     {
