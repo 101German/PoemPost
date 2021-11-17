59ad21fe43f856d50dfb0104ff23ccfa8d8a3fd1
101German <gervika@list.ru>
2021-11-17 13:02:52 +0300

    Add subscription profile

---
+++ PoemPost.Data/Mapping/SubscriptionProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using PoemPost.Data.DTO.Subscription;
+using PoemPost.Data.Models;
+using PoemPost.Data.RequestFeauters;
+
+namespace PoemPost.Data.Mapping
+{
+    public class SubscriptionProfile : Profile
+    {
+        public SubscriptionProfile()
+        {
+            CreateMap<Subscription, SubscriptionDTO>()
+                .ForMember(s => s.AuthorName, opt => opt
+                   .MapFrom(s => s.Author.Name));
+           
+            CreateMap<PagedList<Subscription>, PagedList<SubscriptionDTO>>()
+              .ForMember(p => p.MetaData, opt => opt
+              .MapFrom(p => p.MetaData))
+              .ForMember(p => p.Items, opt => opt.MapFrom(p => p.Items));
+
+            CreateMap<SubscriptionForCreateDTO, Subscription>();
+        }
+    }
+}
