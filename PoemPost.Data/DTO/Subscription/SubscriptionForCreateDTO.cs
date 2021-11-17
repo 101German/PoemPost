aa2759b73fa994ec89f091fb7e5b4c9e194a0b2b
101German <gervika@list.ru>
2021-11-17 12:59:01 +0300

    Add subscription DTO

---
+++ PoemPost.Data/DTO/Subscription/SubscriptionForCreateDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace PoemPost.Data.DTO.Subscription
+{
+    public class SubscriptionForCreateDTO
+    {
+        public int AuthorId { get; set; }
+        public int UserId { get; set; }
+    }
+}
