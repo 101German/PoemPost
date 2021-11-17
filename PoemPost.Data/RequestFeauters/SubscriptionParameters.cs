f4b16674fe6cad80b7f0963db0e039cbb194d5c7
101German <gervika@list.ru>
2021-11-17 13:04:52 +0300

    Add subscription parameters

---
+++ PoemPost.Data/RequestFeauters/SubscriptionParameters.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoemPost.Data.RequestFeauters
+{
+    public class SubscriptionParameters : OrderParameters
+    {
+        public int UserId { get; set; }
+    }
+}
