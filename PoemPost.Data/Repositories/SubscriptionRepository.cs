9578c9c72091da1754ac1089014da3117fc961eb
101German <gervika@list.ru>
2021-11-17 13:04:14 +0300

    Add subscription repository

---
+++ PoemPost.Data/Repositories/SubscriptionRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PoemPost.Data.Interfaces;
+using PoemPost.Data.Models;
+using PoemPost.Data.RequestFeauters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoemPost.Data.Repositories
+{
+    public class SubscriptionRepository : BaseRepository<Subscription>,ISubscriptionRepository
+    {
+        public SubscriptionRepository(RepositoryContext RepositoryContext) : base(RepositoryContext)
+        {
+
+        }
+
+        public async Task<PagedList<Subscription>> GetWithFiltersAsync(SubscriptionParameters subscriptionParameters,bool trackChanges)
+        {
+            var subscriptions = await RepositoryContext.Subscriptions
+                .Where(s => s.UserId == subscriptionParameters.UserId)
+                .ToListAsync();
+
+            return PagedList<Subscription>.ToPagedList(subscriptions, subscriptionParameters.PageNumber, subscriptionParameters.PageSize);
+        }
+    }
+}
