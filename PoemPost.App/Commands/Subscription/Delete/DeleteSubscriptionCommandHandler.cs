1ab3b44e704fe3e91003b204144786e7f65740da
101German <gervika@list.ru>
2021-11-17 12:57:07 +0300

    Add commands for subscription

---
+++ PoemPost.App/Commands/Subscription/Delete/DeleteSubscriptionCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using PoemPost.Data.Interfaces;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PoemPost.App.Commands.Delete
+{
+    public class DeleteSubscriptionCommandHandler : IRequestHandler<DeleteSubscriptionCommand>
+    {
+        private readonly ISubscriptionRepository _subscriptionRepository;
+
+        public DeleteSubscriptionCommandHandler(ISubscriptionRepository subscriptionRepository)
+        {
+            _subscriptionRepository = subscriptionRepository;
+        }
+        public async Task<Unit> Handle(DeleteSubscriptionCommand request, CancellationToken cancellationToken)
+        {
+            var subscriptionEntity = await _subscriptionRepository.GetByIdAsync(request.Id, trackChanges: true);
+
+            _subscriptionRepository.Delete(subscriptionEntity);
+            await _subscriptionRepository.SaveAsync();
+
+            return Unit.Value;
+        }
+    }
+}
