0cc48da1139c9cf53157854d9fa1457ad7f4329c
101German <gervika@list.ru>
2021-11-17 13:04:32 +0300

    Modify paged list

--- PoemPost.Data/RequestFeauters/PagedList.cs
+++ PoemPost.Data/RequestFeauters/PagedList.cs
@@ -4,10 +4,15 @@ using System.Linq;
 
 namespace PoemPost.Data.RequestFeauters
 {
-    public class PagedList<T> :List<T>
+    public class PagedList<T> : List<T>
     {
         public MetaData MetaData { get; set; }
+        public List<T> Items { get; set; }
 
+        public PagedList()
+        {
+
+        }
         public PagedList(List<T> items, int count, int pageNumber, int pageSize)
         {
             MetaData = new MetaData
@@ -18,7 +23,7 @@ namespace PoemPost.Data.RequestFeauters
                 TotalPages = (int)Math.Ceiling(count / (double)pageSize)
             };
 
-            AddRange(items);
+            Items = items;
         }
 
         public static PagedList<T> ToPagedList(IEnumerable<T> source, int pageNumber, int pageSize)
