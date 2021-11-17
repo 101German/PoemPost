a82ef4cf54456425aaab1d20d2f0710a20318af3
101German <gervika@list.ru>
2021-11-17 13:01:40 +0300

    Change AuthorRepository interface

--- PoemPost.Data/Interfaces/IAuthorRepository.cs
+++ PoemPost.Data/Interfaces/IAuthorRepository.cs
@@ -1,10 +1,11 @@
 ﻿using PoemPost.Data.Models;
-
+using PoemPost.Data.RequestFeauters;
+using System.Threading.Tasks;
 
 namespace PoemPost.Data.Interfaces
 {
     public interface IAuthorRepository : IBaseRepository<Author>
     {
-
+        Task<PagedList<Author>> GetAuthors(AuthorParameters authorParameters, bool trackChanges);
     }
 }
