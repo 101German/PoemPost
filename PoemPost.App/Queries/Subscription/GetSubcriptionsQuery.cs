f7f4b3e03acf877c7ee47ca5338c1200c49b09d0
101German <gervika@list.ru>
2021-11-17 12:57:45 +0300

    Add queries for subscription

---
+++ PoemPost.App/Queries/Subscription/GetSubcriptionsQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using PoemPost.Data.DTO.Subscription;
+using PoemPost.Data.RequestFeauters;
+
+namespace PoemPost.App.Queries.Subscription
+{
+    public class GetSubcriptionsQuery : IRequest<PagedList<SubscriptionDTO>>
+    {
+        public bool TrackChanges { get; set; }
+        public SubscriptionParameters SubscriptionParameters { get; set; }
+    }
+}
---
+++ PoemPost.App/Queries/Subscription/GetSubcriptionsQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using MediatR;
+using PoemPost.Data.DTO.Subscription;
+using PoemPost.Data.Interfaces;
+using PoemPost.Data.RequestFeauters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PoemPost.App.Queries.Subscription
+{
+    public class GetSubcriptionsQueryHandler : IRequestHandler<GetSubcriptionsQuery, PagedList<SubscriptionDTO>>
+    {
+        private readonly IMapper _mapper;
+        private readonly ISubscriptionRepository _subscriptionRepository;
+
+        public GetSubcriptionsQueryHandler(IMapper mapper,ISubscriptionRepository subscriptionRepository)
+        {
+            _mapper = mapper;
+            _subscriptionRepository = subscriptionRepository;
+        }
+        public async Task<PagedList<SubscriptionDTO>> Handle(GetSubcriptionsQuery request, CancellationToken cancellationToken)
+        {
+            var subscriptionEntities = await _subscriptionRepository.GetWithFiltersAsync(request.SubscriptionParameters, request.TrackChanges);
+
+            return _mapper.Map<PagedList<SubscriptionDTO>>(subscriptionEntities);
+        }
+    }
+}
