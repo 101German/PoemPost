75cec73358b01f331c796f6e2fbb6ebb9cef2737
101German <gervika@list.ru>
2021-11-18 10:09:28 +0300

    Update migration

--- PoemPost.Host/Migrations/20211117122743_add_category_entity.Designer.cs
+++ PoemPost.Host/Migrations/20211117122743_add_category_entity.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PoemPost.Data;
 
 namespace PoemPost.Host.Migrations
 {
     [DbContext(typeof(RepositoryContext))]
-    partial class RepositoryContextModelSnapshot : ModelSnapshot
+    [Migration("20211117122743_add_category_entity")]
+    partial class add_category_entity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
