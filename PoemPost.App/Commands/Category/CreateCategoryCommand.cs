e0c45362ce9d0deb7b31972deff839a01bec43f6
101German <gervika@list.ru>
2021-11-18 10:02:49 +0300

    Add category commands

---
+++ PoemPost.App/Commands/Category/CreateCategoryCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using PoemPost.Data.DTO.Category;
+
+namespace PoemPost.App.Commands
+{
+    public class CreateCategoryCommand : IRequest<CategoryDTO>
+    {
+        public CategoryForCreationDTO Category { get; set; }
+    }
+}
---
+++ PoemPost.App/Commands/Category/CreateCategoryCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using MediatR;
+using PoemPost.Data.DTO.Category;
+using PoemPost.Data.Interfaces;
+using PoemPost.Data.Models;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PoemPost.App.Commands
+{
+    public class CreateCategoryCommandHandler : IRequestHandler<CreateCategoryCommand, CategoryDTO>
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+        public CreateCategoryCommandHandler(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+        public async Task<CategoryDTO> Handle(CreateCategoryCommand request, CancellationToken cancellationToken)
+        {
+            var categoryEntity = _mapper.Map<Category>(request.Category);
+            _categoryRepository.Insert(categoryEntity);
+            await _categoryRepository.SaveAsync();
+
+            return _mapper.Map<CategoryDTO>(categoryEntity);
+        }
+    }
+}
