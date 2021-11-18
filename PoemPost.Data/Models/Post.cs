3bfa5f6a24a9e9e0e03f14be00431a3f8f450898
101German <gervika@list.ru>
2021-11-18 10:07:19 +0300

    Modify post model

--- PoemPost.Data/Models/Post.cs
+++ PoemPost.Data/Models/Post.cs
@@ -7,13 +7,12 @@ namespace PoemPost.Data.Models
     {
         public string Title { get; set; }
         public string PoemText { get; set; }
-
         public int AuthorId { get; set; }
+        public int CategoryId { get; set; }
         public virtual Author Author { get; set; }
-
+        public virtual Category Category { get; set; }
         public virtual ICollection<Comment> Comments { get; set; }
         public virtual ICollection<Like> Likes { get; set; }
-
         public DateTime CreationDate { get; set; }
         public DateTime? LastUpdateDate { get; set; }
 
