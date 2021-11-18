e7c09ef9b6b89c5bd64a6138e460fae65e27bf3f
101German <gervika@list.ru>
2021-11-18 10:07:47 +0300

    Implement CategoryRepository interface

---
+++ PoemPost.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using PoemPost.Data.Interfaces;
+using PoemPost.Data.Models;
+
+namespace PoemPost.Data.Repositories
+{
+    public class CategoryRepository : BaseRepository<Category>,ICategoryRepository
+    {
+        public CategoryRepository(RepositoryContext repositoryContext) : base(repositoryContext)
+        {
+
+        }
+    }
+}
