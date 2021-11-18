285dff95bd3860d5aafac97da9e32315402b63d8
101German <gervika@list.ru>
2021-11-18 10:07:04 +0300

    Add category model

---
+++ PoemPost.Data/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace PoemPost.Data.Models
+{
+    public class Category : BaseEntity
+    {
+        public string Name { get; set; }
+        public virtual ICollection<Post> Posts { get; set; }
+    }
+}
