6a1090cda1d1dcb8f8c79e7a256210c82fdecf85
101German <gervika@list.ru>
2021-11-26 16:37:43 +0300

    Add user context

---
+++ PoemPost.Data/UserContext/IUserContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PoemPost.Data.UserContext
+{
+    public interface IUserContext
+    {
+        Guid UserId { get; set; }
+        string Name { get; set; }
+        string Email { get; set; }
+        int AuthorId { get; set; }
+    }
+}
