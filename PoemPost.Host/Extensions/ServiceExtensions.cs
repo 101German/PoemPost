123c3ae59d7df0334edce944c186b52b2d1b2e11
101German <gervika@list.ru>
2021-11-26 16:38:35 +0300

    Modify service extensions

--- PoemPost.Host/Extensions/ServiceExtensions.cs
+++ PoemPost.Host/Extensions/ServiceExtensions.cs
@@ -1,12 +1,60 @@
-﻿using MassTransit;
+﻿using IdentityServer4.AccessTokenValidation;
+using MassTransit;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using PoemPost.Data.Settings;
+using PoemPost.Data.UserContext;
+using System.Collections.Generic;
 
 namespace PoemPost.Host.Extensions
 {
     public static class ServiceExtensions
     {
+        public static void ConfigureSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "PoemPost",
+                    Version = "v1"
+                });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Place to add JWT with Bearer",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference =new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        },
+                        Scheme = "oauth2",
+                        Name = "Bearer",
+                        In = ParameterLocation.Header
+                    },
+                    new List<string>()
+                    }
+                });
+            });
+        }
+
         public static void ConfigureCors(this IServiceCollection services) =>
-           services.AddCors(options => options.AddPolicy("CorsPolicy", builder => builder
+           services.AddCors(options => options
+           .AddPolicy("CorsPolicy", builder => builder
            .AllowAnyOrigin()
            .AllowAnyMethod()
            .AllowAnyHeader()
@@ -21,6 +69,24 @@ namespace PoemPost.Host.Extensions
             });
 
             services.AddMassTransitHostedService();
+        }
+
+        public static void AddAuthenticationSettings(this IServiceCollection services, IConfiguration configuration)
+        {
+            Authentication authSettings = configuration.GetSection("Authentication").Get<Authentication>();
+
+            services.AddAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme).AddIdentityServerAuthentication(options =>
+            {
+                options.Authority = authSettings.Authority;
+                options.RequireHttpsMetadata = authSettings.RequireHttpsMetadata;
+                options.ApiName = authSettings.Audience;
+                options.SaveToken = true;
+            });
+        }
+
+        public static void AddUserContext(this IServiceCollection services)
+        {
+            services.AddScoped<IUserContext, UserContext>();
         }
     }
 }
