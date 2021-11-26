af7338d62504e79c6034d91d5ac00d72ca054af4
101German <gervika@list.ru>
2021-11-26 16:34:16 +0300

    Update postProcessor

--- PoemPost.App/PostProcessors/CreatePostPostProcessor.cs
+++ PoemPost.App/PostProcessors/CreatePostPostProcessor.cs
@@ -1,7 +1,9 @@
 ﻿using MassTransit;
-using MediatR;
 using MediatR.Pipeline;
+using Models;
 using PoemPost.Data.DTO;
+using PoemPost.Data.Interfaces;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -10,15 +12,31 @@ namespace PoemPost.App.Commands.Create
     public class CreatePostPostProcessor : IRequestPostProcessor<CreatePostCommand, PostDTO>
     {
         private readonly IPublishEndpoint _publishEndpoint;
+        private readonly ISubscriptionRepository _subscriptionRepository;
 
-        public CreatePostPostProcessor(IPublishEndpoint publishEndpoint)
+        public CreatePostPostProcessor(
+            IPublishEndpoint publishEndpoint,
+            ISubscriptionRepository subscriptionRepository)
         {
             _publishEndpoint = publishEndpoint;
+            _subscriptionRepository = subscriptionRepository;
         }
 
         public async Task Process(CreatePostCommand request, PostDTO response, CancellationToken cancellationToken)
         {
-            await _publishEndpoint.Publish(response,cancellationToken);
+            var subscriptions =await _subscriptionRepository.GetByAuthorId(response.AuthorId);
+            var usersForNotify = new UsersForNotify
+            {
+                AuthorName = response.AuthorName,
+                PoemName = response.Title
+            };
+
+            foreach (var email in subscriptions.Select(s=>s.UserEmail))
+            {
+                usersForNotify.UsersEmails.Add(email);
+            }
+
+            await _publishEndpoint.Publish(usersForNotify,cancellationToken);
         }
     }
 }
