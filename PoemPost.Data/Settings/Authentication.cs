5532d230dc23030f9ce78c037c3a4bee6c3f3521
101German <gervika@list.ru>
2021-11-26 16:37:31 +0300

    Add authentication settings

---
+++ PoemPost.Data/Settings/Authentication.cs
@@ -0,0 +1,9 @@
+﻿namespace PoemPost.Data.Settings
+{
+    public class Authentication
+    {
+        public string Authority { get; set; }
+        public bool RequireHttpsMetadata { get; set; }
+        public string Audience { get; set; }
+    }
+}
